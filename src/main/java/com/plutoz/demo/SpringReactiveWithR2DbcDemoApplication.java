c4482dfde5c6f76fcb473ef9776927322cec42e7
László Csatlós <plutoz01@gmail.com>
2021-12-25 15:49:14 +0100

    spring initializr initial content

---
+++ src/main/java/com/plutoz/demo/SpringReactiveWithR2DbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.plutoz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWithR2DbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveWithR2DbcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plutoz/demo/SpringReactiveWithR2DbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plutoz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveWithR2DbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
